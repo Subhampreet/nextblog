0603d3919192ac3c699e5c6c174cd8bba09869d3
Subhampreet <subhampreet18@gmail.com>
2024-08-25 10:50:28 +0530

    Build 0.0.5 : Suspense and Streaming 🦔

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React from 'react'
 
 export default async function page() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
     const response = await fetch("https://dummyjson.com/posts?limit=10");
     const data = await response.json();
     return (
