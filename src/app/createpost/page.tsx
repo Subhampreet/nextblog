c5fddd867b534351dc6108d1a7ca535d694ab2d1
Subhampreet <subhampreet18@gmail.com>
2024-08-28 23:51:39 +0530

    Build 0.0.8 : Server Action Added

--- src/app/createpost/page.tsx
+++ src/app/createpost/page.tsx
@@ -1,10 +1,13 @@
+// 'use server'
+
+import { createPost } from '@/actions/actions'
 import React from 'react'
 
 export default function page() {
   return (
     <div className='text-center pt-16'>
         <h1 className='text-4xl md:text-5xl font-bold mb-5'>Create Post</h1>
-        <form className='h-10 space-x-2 mt-10'>
+        <form action={createPost} className='h-10 space-x-2 mt-10'>
             <input type="text" name='title' placeholder='enter title for new post' className='border rounded px-3 h-full mr-2' required />
             <button className='h-full bg-black px-5 rounded text-white'>Submit</button>
         </form>
