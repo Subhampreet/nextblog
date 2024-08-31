16fbe28195924153733e2065497be3bc7d4f5eab
Subhampreet <subhampreet18@gmail.com>
2024-08-31 19:04:52 +0530

    Build 0.1.3 : DB Connectivity Added + Create Post backend 🏴‍☠️

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,6 +1,18 @@
 "use server"
 
+import prisma from "@/lib/db";
+
+
 export async function createPost(formData: FormData) {
     const title = formData.get("title") as string;
     const body = formData.get("body") as string;
+
+    // update database
+  await prisma.post.create({
+    data: {
+      title,
+      body,
+    },
+  });
+
 }
